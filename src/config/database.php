58941f6fc2301231681643d59af4142219a5f861
Greg Hoole <greg@ultimatenewmedia.com>
2020-04-03 11:31:55 -0400

    changing database name in config/database.php

--- src/config/database.php
+++ src/config/database.php
@@ -83,7 +83,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE_CMS', 'SurfsUpCMS'),
+            'database' => env('DB_DATABASE_CMS', 'SetToSootheCMS'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', 'root'),
 			'charset'   => 'utf8mb4',
