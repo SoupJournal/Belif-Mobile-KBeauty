c5b3a9cc680fdefb2d8e0932b3f2b51934f8d48e
Greg Hoole <greg@ultimatenewmedia.com>
2020-08-14 11:49:09 -0400

    fb pixel and ga tags

--- src/packages/site/src/views/general/scripts.blade.php
+++ src/packages/site/src/views/general/scripts.blade.php
@@ -52,27 +52,31 @@
 	
 	{{-- Google Analytics --}}
 	<!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-145757171-1"></script>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-91272304-4"></script>
 	<script>
-	 window.dataLayer = window.dataLayer || [];
-	 function gtag(){dataLayer.push(arguments);}
-	 gtag('js', new Date());
-
-	 gtag('config', '');
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+		gtag('config', 'UA-91272304-4');
 	</script>
 
 	{{-- Facebook Pixel Code --}}
 	<script>
-		!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-				n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
-			n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
-			t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
-				document,'script','https://connect.facebook.net/en_US/fbevents.js');
-		fbq('init', '\'); // Insert your pixel ID here.
+		!function(f,b,e,v,n,t,s)
+		{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+				n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+			if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+			n.queue=[];t=b.createElement(e);t.async=!0;
+			t.src=v;s=b.getElementsByTagName(e)[0];
+			s.parentNode.insertBefore(t,s)}(window,document,'script',
+				'https://connect.facebook.net/en_US/fbevents.js');
+		fbq('init', '775599159857903');
 		fbq('track', 'PageView');
 	</script>
 	<noscript>
-		<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=&ev=PageView&noscript=1"/>
+		<img height="1" width="1"
+			 src="https://www.facebook.com/tr?id=775599159857903&ev=PageView
+	&noscript=1"/>
 	</noscript>
 	{{-- DO NOT MODIFY --}}
 	{{-- End Facebook Pixel Code --}}
