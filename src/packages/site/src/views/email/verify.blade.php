7661112fc40d084a92866371f65dfacf08f3fd80
Greg Hoole <greg@ultimatenewmedia.com>
2020-08-18 05:41:23 -0400

    update confirm header

--- src/packages/site/src/views/email/verify.blade.php
+++ src/packages/site/src/views/email/verify.blade.php
@@ -55,7 +55,7 @@
 															<td style="border: 0; margin: 0; padding: 0; font-size:0; line-height:0;" height="107"></td>
 															<td style="border: 0; margin: 0; padding: 0; font-size:0; line-height:0;" valign="middle" align="center"><img src="https://soup-journal-app-storage.s3.amazonaws.com/aloeaquabomb/belif_stamp.png" width="100" alt=""/></td>
 															<td style="border: 0; margin: 0; padding: 0; font-size:0; line-height:0;" align="center" valign="middle"><img src="https://soup-journal-app-storage.s3.amazonaws.com/belif/Surfsup/BELIF_LOGO.png" width="150" alt="Belif"/></td>
-															<td style="border: 0; margin: 0; padding: 0; font-size:0; line-height:0;" valign="middle" align="center"><img src="https://soup-journal-app-storage.s3.amazonaws.com/aloeaquabomb/belif_sts_top.png" width="100" alt=""/></td>
+															<td style="border: 0; margin: 0; padding: 0; font-size:0; line-height:0;" valign="middle" align="center"><img src="https://soup-journal-app-storage.s3.amazonaws.com/aqualand/aqua_confirm_header.png" width="100" alt=""/></td>
 															<td style="border: 0; margin: 0; padding: 0; font-size:0; line-height:0;"></td>
 														</tr>
 														<tr>
