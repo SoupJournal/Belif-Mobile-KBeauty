3d27a17bd96345f0cb065ac27bd70366403fd7c2
Greg Hoole <greg@ultimatenewmedia.com>
2020-08-20 11:38:55 -0400

    emails

--- src/packages/site/src/views/email/verify.blade.php
+++ src/packages/site/src/views/email/verify.blade.php
@@ -128,8 +128,8 @@
 						</tr>
 						<tr>
 							<td style="border: 0; margin: 0 ; padding: 0; text-align: center;" valign="top">
-								<div>
-									<table width="570" border="0" cellspacing="0" cellpadding="0" style="background: url('https://soup-journal-app-storage.s3.amazonaws.com/aqualand/belif_Aqualand-Quiz_backgrounds_Page-1.jpg') no-repeat bottom center; background-size: cover;">
+								<div style="height: 750px;background: url('https://soup-journal-app-storage.s3.amazonaws.com/aqualand/belif_Aqualand-Quiz_backgrounds_Page-1.jpg') no-repeat bottom center; background-size: cover;">
+									<table width="570" border="0" cellspacing="0" cellpadding="0">
 										<tbody>
 											<tr>
 												<td height="40" align="center" valign="middle" style="border: 0; margin: 0; padding: 0;"></td>
