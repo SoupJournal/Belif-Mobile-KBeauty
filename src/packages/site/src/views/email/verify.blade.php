d286a241cf06f29767262c29e7e640993f221225
Greg Hoole <greg@ultimatenewmedia.com>
2020-12-04 13:49:19 -0500

    email updates

--- src/packages/site/src/views/email/verify.blade.php
+++ src/packages/site/src/views/email/verify.blade.php
@@ -121,7 +121,7 @@
 											</td>
 										</tr>
 										<tr>
-											<td align="center" valign="middle" style="border: 0; margin: 0; padding: 0; font-family: {{ $fontDefinitions }}; color:#ffffff; font-size:12px; background-color: #01b9ff;">{!! $html !!}</td>
+											<td align="center" valign="middle" style="border: 0; margin: 0; padding: 0; font-family: {{ $fontDefinitions }}; color:#ffffff; font-size:12px; ">{!! $html !!}</td>
 										</tr>
 									</tbody>
 								</table>
@@ -129,7 +129,7 @@
 						</tr>
 						<tr>
 							<td style="border: 0; margin: 0 ; padding: 0; text-align: center;" valign="top">
-								<div style="background: url('https://soup-journal-app-storage.s3.amazonaws.com/aqualand/belif_Aqualand-Quiz_backgrounds_Verify_Email.jpg') no-repeat bottom center; background-size: cover;">
+								<div style="">
 									<table width="570" border="0" cellspacing="0" cellpadding="0">
 										<tbody>
 											<tr>
@@ -219,7 +219,7 @@
 													<td bgcolor="#ff76a7" style="border:0; margin:0; padding: 5px 10px 8px 10px; text-align:center; font-family:{{ $fontDefinitions }}; color:#ffffff; font-size:12px; line-height:18px; font-weight: bold; letter-spacing:-0.4px; word-spacing: -0.2px;">We'll send you an email once<br/>the sample has been shipped!</td>
 												</tr>
 												<tr>
-													<td align="center"><img src="https://soup-journal-app-storage.s3.amazonaws.com/aqualand/aqualand_products.png" width="200" /></td>
+													<td align="center"></td>
 												</tr>
 												</tbody>
 											</table>
@@ -231,37 +231,14 @@
 						</tr>
 						{{-- Social Links Below --}}
 						<tr>
-							<td align="center" valign="top" bgcolor="#01b9ff" style="background-color: #01b9ff; border: 0; margin: 0; padding: 0;">
-								<table width="570" border="0" cellspacing="0" cellpadding="0">
-									<tbody>
-									<tr>
-										<td colspan="3" height="14" align="center" valign="middle" style="border: 0; margin: 0; padding: 0; background-color: #01b9ff; line-height: 0;"></td>
-									</tr>
-									<tr>
-										<td align="center" valign="middle" style="width: 45%; border: 0; margin: 0; padding: 0; background-color: #01b9ff; color: #fff; text-align: right; font-family:{{ $fontDefinitions }};">
-											<a href="https://belifusa.com" style="color: #fff; font-style: italic; text-decoration: none;">belifusa.com</a>
-										</td>
-										<td style="width: 10%;">&nbsp;</td>
-										<td align="center" valign="middle" style="width: 45%; border: 0; margin: 0; padding: 0; background-color: #01b9ff; color: #fff; text-align: left; font-family:{{ $fontDefinitions }};">
-											<a href="#" style="color: #fff; font-style: italic; text-decoration: none; white-space: nowrap;">belif at sephora</a>
-										</td>
-									</tr>
-									<tr>
-										<td colspan="3" height="14" align="center" valign="middle" style="border: 0; margin: 0; padding: 0; background-color: #01b9ff; line-height: 0;"></td>
-									</tr>
-									</tbody>
-								</table>
-							</td>
-						</tr>
-						<tr>
-							<td align="center" valign="top" bgcolor="#01b9ff" style="background-color: #01b9ff; border: 0; margin: 0; padding: 0;">
+							<td align="center" valign="top" bgcolor="#01b9ff" style=" border: 0; margin: 0; padding: 0;">
 								<table width="570" border="0" cellspacing="0" cellpadding="0">
 									<tbody>
 										<tr>
-											<td height="19" align="center" valign="middle" style="border: 0; margin: 0; padding: 0; background-color: #01b9ff;">&nbsp;</td>
+											<td height="19" align="center" valign="middle" style="border: 0; margin: 0; padding: 0; ">&nbsp;</td>
 										</tr>
 										<tr>
-											<td width="570" align="center" valign="middle" style="border: 0; margin: 0; padding: 0; background-color: #01b9ff; color: #fff; text-align: center; font-family:{{ $fontDefinitions }}; text-align: center;">
+											<td width="570" align="center" valign="middle" style="border: 0; margin: 0; padding: 0;  color: #fff; text-align: center; font-family:{{ $fontDefinitions }}; text-align: center;">
 												<a href="https://www.instagram.com/belifusa/" target="_blank"><img src="https://soup-journal-app-storage.s3.amazonaws.com/aqualand/instagram-logo-transparent.png" width="27" height="27" alt="Belif Instagram" style="border: 0; margin: 0; padding: 0;"/></a>
 												&nbsp; &nbsp; &nbsp;
 												<a href="https://www.facebook.com/belifUSA/" target="_blank"><img src="https://soup-journal-app-storage.s3.amazonaws.com/aqualand/facebook-logo-transparent.png" width="27" height="27" alt="Belif Facebook" style="border: 0; margin: 0; padding: 0;"/></a>
@@ -270,31 +247,17 @@
 											</td>
 										</tr>
 										<tr>
-											<td height="14" align="center" valign="middle" style="border: 0; margin: 0; padding: 0; background-color: #01b9ff; line-height: 0;">&nbsp;</td>
+											<td height="14" align="center" valign="middle" style="border: 0; margin: 0; padding: 0;  line-height: 0;">&nbsp;</td>
 										</tr>
 										<tr>
-											<td align="center" valign="middle" style="border: 0; margin: 0; padding: 0; background-color: #01b9ff; color: #fff; font-family:{{ $fontDefinitions }}; font-size:14px; line-height: 20px; font-style:italic; text-align: center;">&copy; belif cosmetics</td>
+											<td align="center" valign="middle" style="border: 0; margin: 0; padding: 0;  color: #fff; font-family:{{ $fontDefinitions }}; font-size:14px; line-height: 20px; font-style:italic; text-align: center;">&copy; 2017 LG Household & Health Care, LTD. All Rights Reserved.</td>
 										</tr>
 										<tr>
-											<td height="9" align="center" valign="middle" style="border: 0; margin: 0; padding: 0; background-color: #01b9ff;">&nbsp;</td>
+											<td height="9" align="center" valign="middle" style="border: 0; margin: 0; padding: 0; ">&nbsp;</td>
 										</tr>
 									</tbody>
 								</table>
 							</td>
-						</tr>
-						<tr>
-							<td height="8" style="border: 0; margin: 0; padding: 0;"></td>
-						</tr>
-						<tr>
-							<td style="border: 0; margin: 0; padding: 0; text-align:center; font-family:{{ $fontDefinitions }}; font-size:14px; line-height: 20px; font-style:italic;">
-								<unsubscribe> Need to change your address or report spam?<br/>Contact us at <a href="mailto:contact@belifinhydration.com" target="_top">contact@belifinhydration.com</a></unsubscribe>
-							</td>
-						</tr>
-                        <tr>
-							<td style="border: 0; margin: 0; padding: 0; text-align:center; font-family:{{ $fontDefinitions }}; font-size:14px; line-height: 20px; font-style:italic;"><a href="{{ $unsubscribeLink }}" style="text-decoration:none; color:#000000;"><unsubscribe>unsubscribe</unsubscribe></a></td>
-						</tr>
-						<tr>
-							<td height="8" style="border: 0; margin: 0; padding: 0;"></td>
 						</tr>
 					</tbody>
 			</table></td>
