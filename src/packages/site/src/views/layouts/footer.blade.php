8baf13674504dfb4b7641da048680728d240459a
Greg Hoole <greg@ultimatenewmedia.com>
2020-12-07 16:25:25 -0500

    remove footer

--- src/packages/site/src/views/layouts/footer.blade.php
+++ src/packages/site/src/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 {{-- footer --}}
 <div class="page-footer color-2">
-	<div class="email"><a href="mailto:service@belifusa.com">service@belifusa.com</a></div>
-	<div class="copyright">&copy; belif cosmetics</div>
+{{--	<div class="email"><a href="mailto:service@belifusa.com">service@belifusa.com</a></div>--}}
+{{--	<div class="copyright">&copy; belif cosmetics</div>--}}
 </div>
